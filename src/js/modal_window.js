c5d8bbf5e0dce810bae867fe1d55e7a199831b7f
IgorShchegel <mitosss777@gmail.com>
2024-02-08 20:39:47 +0200

    modal2

--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -2,33 +2,45 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from 'axios';
 
-async function fetchImages(query) {
-  const key = '41984486-af85e40ac9c664bf86aaf37aa';
+const card = document.querySelector('.modal');
+const button = document.querySelector('.search-button');
 
-  const url = `https://pixabay.com/api/?`;
+button.addEventListener('click', modalcard);
+
+async function fetchImages() {
+  const url = `https://energyflow.b.goit.study/api/exercises/64f389465ae26083f39b17a4`;
 
   try {
-    const response = await axios.get(url, {
-      params: {
-        key: key,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        page: page,
-        per_page: 40,
-        safesearch: true,
-      },
-    });
-    if (response.data.hits.length === 0) {
-      searchForm.reset();
-      hideButton();
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    } else {
-      return response.data;
-    }
-  } catch (error) {
-    throw new Error(error);
-  }
+    const response = await axios.get(url);
+
+    return response.data;
+  } catch (error) {}
+}
+
+function displayImages(cardObj) {
+  card.innerHTML = ` <div>
+  <img src="${cardObj.gifUrl}"/>
+  </div>
+  <div>
+  <h3>${cardObj.name}</h3>
+  <div>
+  <p>${cardObj.rating}</p>
+  </div>
+  <ul>
+  <li>${cardObj.target}</li>
+  <li>${cardObj.bodyPart}</li>
+  <li>${cardObj.equipment}</li>
+  <li>${cardObj.popularity}</li>
+  <li>${cardObj.burnedCalories}/${cardObj.time} min</li>
+  </ul>
+  <p>${cardObj.description}</p>
+  <button type="button">Add to favorites</button>
+  </div>  `;
+}
+
+async function modalcard() {
+  try {
+    const cardObj = await fetchImages();
+    displayImages(cardObj);
+  } catch (error) {}
 }

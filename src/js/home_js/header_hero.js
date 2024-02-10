13d61d7ac2c534513c568daf3db4344ba45f15b4
sheremetiewaliena <142352568+sheremetiewaliena@users.noreply.github.com>
2024-02-10 19:10:38 +0200

    mark up

---
+++ src/js/home_js/header_hero.js
@@ -0,0 +1,6 @@
+const homeEl = document.querySelector('.Home');
+const favoritesEl = document.querySelector('.Favorites');
+
+favoritesEl.addEventListener('click', onClickHandle);
+
+function onClickHandle(event) {}

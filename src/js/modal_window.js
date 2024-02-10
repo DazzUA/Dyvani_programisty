68fd469a4c24ebd226c3d318f75911b383266cfb
IgorShchegel <mitosss777@gmail.com>
2024-02-10 18:04:47 +0200

    modal4

--- src/js/modal_window.js
+++ src/js/modal_window.js
@@ -2,12 +2,59 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from 'axios';
 
-const modalBackdrop = document.querySelector('.backdrop');
-const card = document.querySelector('.modal');
-const button = document.querySelector('.search-button');
-const hiddenClass = 'is-hidden';
+const modalBackdrop = document.querySelector('.Backdrop');
+const card = document.querySelector('.Modal');
+const button = document.querySelector('.SearchButton');
+const modalClose = document.querySelector('.ModalClose');
+const addRemoveFavorites = document.querySelector('.AddRemoveFavorites');
+const openClass = 'IsOpen';
+let cardObj = {};
 
-button.addEventListener('click', modalcard);
+button.addEventListener('click', modalCard);
+
+async function modalCard() {
+  try {
+    cardObj = await fetchImages();
+    showModal();
+    displayImages(cardObj);
+    addRemoveFavorites.addEventListener('submit', addFavorites);
+    modalClose.addEventListener('click', function (event) {
+      if (event.target === modalClose) {
+        hideModal();
+      }
+    });
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        hideModal();
+      }
+    });
+
+    modalBackdrop.addEventListener('click', function (event) {
+      if (event.target === modalBackdrop) {
+        hideModal();
+      }
+    });
+  } catch (error) {}
+}
+
+function modalCloseFunc() {
+  modalClose.addEventListener('click', function (event) {
+    if (event.target === modalClose) {
+      hideModal();
+    }
+  });
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      hideModal();
+    }
+  });
+
+  modalBackdrop.addEventListener('click', function (event) {
+    if (event.target === modalBackdrop) {
+      hideModal();
+    }
+  });
+}
 
 async function fetchImages() {
   const url = `https://energyflow.b.goit.study/api/exercises/64f389465ae26083f39b17a4`;
@@ -20,37 +67,51 @@ async function fetchImages() {
 }
 
 function displayImages(cardObj) {
-  card.innerHTML = ` <div>
-  <img class="image-gif" src="${cardObj.gifUrl}" alt="gif"/>
+  card.innerHTML = ` <button class="ModalClose" type="button">
+          <svg class="CloseModalIcon" width="8" height="8">
+            <use href="../img/symbol-defs.svg#icon-close"></use>
+          </svg>
+        </button>
+  <div class="ModalImage">     
+  <img class="ImageGif" src="${cardObj.gifUrl}" alt="imagegif"/>
   </div>
-  <div>
-  <h3>${cardObj.name}</h3>
-  <div>
-  <p>${cardObj.rating}</p>
+  <h3 class="ModalTitle">${cardObj.name}</h3>
+  <div class="ModalRating">
+  <p class="NumberRating">${cardObj.rating}</p>
+  <div class="StarRating"></div>
   </div>
-  <ul class="modal-list">
-  <li>${cardObj.target}</li>
-  <li>${cardObj.bodyPart}</li>
-  <li>${cardObj.equipment}</li>
-  <li>${cardObj.popularity}</li>
-  <li>${cardObj.burnedCalories}/${cardObj.time} min</li>
+  <ul class="ModalList">
+  <li class="ModalListItem">Target ${cardObj.target}</li>
+  <li class="ModalListItem">Body Part ${cardObj.bodyPart}</li>
+  <li class="ModalListItem">Equipment${cardObj.equipment}</li>
+  <li class="ModalListItem">Popular${cardObj.popularity}</li>
+  <li class="ModalListItem">Burned Calories${cardObj.burnedCalories}/${cardObj.time} min</li>
   </ul>
-  <p class="description">${cardObj.description}</p>
-  <button type="button">Add to favorites</button>
+  <p class="Description">${cardObj.description}</p>
+  <button class="AddRemoveFavorites" type="button">Add to favorites</button>
   </div>  `;
 }
 
-async function modalcard() {
-  try {
-    const cardObj = await fetchImages();
-    displayImages(cardObj);
-  } catch (error) {}
+function addFavorites() {
+  addRemoveFavorites.innerText = 'Remove from';
 }
 
-function hideButton() {
-  modalBackdrop.classList.add(hiddenClass);
+function showModal() {
+  modalBackdrop.classList.add(openClass);
 }
 
-function showButton() {
-  modalBackdrop.classList.remove(hiddenClass);
+function hideModal() {
+  modalBackdrop.classList.remove(openClass);
 }
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    close();
+  }
+});
+
+modalBackdrop.addEventListener('click', function (event) {
+  if (event.target === modalBackdrop) {
+    close();
+  }
+});

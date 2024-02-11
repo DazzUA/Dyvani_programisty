9631216b2d101c428187834c8b69593e8b74b609
sheremetiewaliena <142352568+sheremetiewaliena@users.noreply.github.com>
2024-02-11 18:39:15 +0200

    minor changes

--- src/js/home_js/header_hero.js
+++ src/js/home_js/header_hero.js
@@ -1,20 +1,17 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+/*const homeEl = document.querySelector('.Home');
+const favoritesEl = document.querySelector('.Favorites');
 
-// const homeEl = document.querySelector('.Home');
-// const favoritesEl = document.querySelector('.Favorites');
+favoritesEl.addEventListener('click', onClickHandleFavorites);
 
-// favoritesEl.addEventListener('click', onClickHandleFavorites);
+function onClickHandleFavorites() {
+  homeEl.classList.toggle('current');
+  favoritesEl.classList.remove('favorites');
+  favoritesEl.classList.add('current');
+}
 
-// function onClickHandleFavorites() {
-//   homeEl.classList.toggle('current');
-//   favoritesEl.classList.remove('favorites');
-//   favoritesEl.classList.add('current');
-// }
+homeEl.addEventListener('click', onClickHandleHome);
 
-// homeEl.addEventListener('click', onClickHandleHome);
-
-// function onClickHandleHome() {
-//   homeEl.classList.add('current');
-//   favoritesEl.classList.remove('current');
-// }
+function onClickHandleHome() {
+  homeEl.classList.add('current');
+  favoritesEl.classList.remove('current');
+}*/

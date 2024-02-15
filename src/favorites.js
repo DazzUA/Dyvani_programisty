ad63ebacf9706ab6003e2994b04ea0805cc98116
dazzUA <pavelbelozerov18@gmail.com>
2024-02-15 20:32:27 +0200

    change path js

---
+++ src/favorites.js
@@ -0,0 +1,5 @@
+import './js/favorites_js/header_favorites.js';
+import './js/favorites_js/quote_of_the_day_favorives.js';
+import './js/favorites_js/favorites_section.js';
+import './js/modal_window.js';
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/home_js/header_hero.js';
+import './js/home_js/quote_of_the_day.js';
+import './js/home_js/exercises_filters.js';
+import './js/home_js/exercises_subcategories.js';
+import './js/home_js/exercises_search.js';
+import './js/home_js/footer.js';
+import './js/modal_window.js';
